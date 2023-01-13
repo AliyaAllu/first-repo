b678739da62def941895229279c18ed052429fd8
Aliya <ajaliya287@gmail.com>
2023-01-13 17:11:45 +0530

    Add code to display array elements in array.java

--- array.java
+++ array.java
@@ -2,8 +2,27 @@ import java.io.*;
 import java.lang.*;
 class array
 {
-    public static void main(String args[])
-    {
+public static void main(String args[])
+{
+int a[];
+int n,i;
+DataInputStream dis=new DataInputStream(System.in);
+try
+{
+System.out.println("enter the size");
+n=Integer.parseInt(dis.readLine());
+a= new int[n];
+System.out.println("enter the elements");
+for(i=0;i<n;i++)
+{
+a[i]=Integer.parseInt(dis.readLine());
+}
+for(i=0;i<n;i++)
+{
+System.out.println("a[i] =" +a[i]);}
 
-    }
+}catch(IOException e)
+{
+System.out.println(e);
+}}
 }
